8821be988162fa721b03578c92010428762e5107
임기원 <imgiwon@imgiwon-ui-MacBookPro.local>
2024-01-08 11:01:14 +0900

    Initial commit

---
+++ src/main/java/com/ig/amr/AmrApplication.java
@@ -0,0 +1,13 @@
+package com.ig.amr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ig/amr/AmrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ig.amr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
